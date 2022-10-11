42a570fc7366782c38bd3042bc0e75a8a1769240
AlexPronin <pralex1965@gmail.com>
2022-10-11 21:04:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rab8_jetpakc_navi/Fragment2.kt
@@ -0,0 +1,26 @@
+package com.example.rab8_jetpakc_navi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+class Fragment2 : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment2, container, false)
+    }
+
+
+
+
+}
